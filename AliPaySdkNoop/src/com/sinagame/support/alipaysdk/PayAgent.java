c66b953222275173e761fed5471b878568162197
chonghui1 <chonghui1@staff.sina.com.cn>
2016-01-06 19:58:15 +0800

    commit all

--- AliPaySdkNoop/src/com/sinagame/support/alipaysdk/PayAgent.java
+++ AliPaySdkNoop/src/com/sinagame/support/alipaysdk/PayAgent.java
@@ -1,6 +1,30 @@
 package com.sinagame.support.alipaysdk;
 
 public class PayAgent {
-    public PayAgent() {
+
+    public static interface PayStateListener {
+        void onSuccess();
+
+        void onFailure();
+
+        void onRunning();
+
+        void onCancler();
+
+        void onOffLine();
+
+        void onCheckNO();
     }
+
+    public PayAgent(String[] s, PayStateListener l) {
+    }
+
+    protected boolean b(String i) {
+        return true;
+    }
+
+    public void pay(final android.app.Activity act, String arg1, String arg2,
+                    String arg3, String arg4, String arg5) {
+    }
+
 }
